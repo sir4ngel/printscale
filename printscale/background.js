bd42a256cbb2b1c83b83b0c5513329acca16ff1c
Angel Garcia <al051783@uacam.mx>
2021-08-06 23:55:19 -0500

    Cambios severos en la funcionalidad, se agregó auto reconexión

--- printscale/background.js
+++ printscale/background.js
@@ -1,33 +1,53 @@
-var weight = '';
-var serverId = "obmgnbppkiokeaphehaakiobllapmdjh";
-
 chrome.runtime.onConnectExternal.addListener(function (port) {
   port.onMessage.addListener(function (msg) {
-    if(msg.status == 'RELOAD') {
-      chrome.runtime.sendMessage(msg);
-      console.log(msg);
-    }
-    if(msg.status == 'RELOAD-EMPTY') {
-      chrome.runtime.sendMessage(msg);
-      console.log(msg);
-    }
-    if(msg.status == 'WEIGHT') {
-      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, msg, function () {
+    if (msg.status == 'WEIGHT') {
+      if (msg.weight == '') {
+        console.log("Peso muerto");
+      } else {
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+          chrome.tabs.sendMessage(tabs[0].id, msg, function () {
+          });
         });
-      });
+        console.log(msg);
+      }
+    }
+    if (msg.status == 'SUCCESS') {
+      chrome.runtime.sendMessage(msg);
+      console.log(msg);
+    }
+    if (msg.status == 'TEST') {
+      console.log(msg);
     }
   });
 });
 
 chrome.runtime.onMessage.addListener(function (response, sender, sendResponse) {
-  if (response.status == 'RELOAD') {
-    var port = chrome.runtime.connect(serverId);
-    port.postMessage(response);
+  if (response.status == 'SERVERID') {
+    try {
+      chrome.storage.local.get(['serverId'], function (result) {
+        if (typeof result.serverId == 'undefined' || result.serverId === null) {
+          chrome.storage.local.set({ serverId: response.serverId }, function () {
+            try {
+              var clientId = chrome.runtime.id;
+              var port = chrome.runtime.connect(response.serverId);
+              port.postMessage({status: response.status, clientId: clientId});
+            } catch (error) {
+              console.log(error);
+              chrome.storage.local.set({serverId: null}, function() {
+              });
+            }
+          });
+        }
+      });
+    } catch (error) {
+      console.log(error);
+    }
   }
-  if(response.status == 'CONNECT') {
-    var port = chrome.runtime.connect(serverId);
-    port.postMessage(response);
+  if (response.status == 'CHECKIFCONNEXISTS') {
+    chrome.storage.local.get(['serverId'], function(result) {
+      if (typeof result.serverId !== 'undefined' && result.serverId !== null) {
+        chrome.runtime.sendMessage({status: 'EXISTS'});
+      }
+    });
   }
-
 });
--- printscale/popup.js
+++ printscale/popup.js
@@ -1,59 +1,51 @@
-var connect = document.getElementById('connect');
-connect.disabled = true;
+checkIfServerIdExist();
 
-reload.addEventListener("click", async () => {
-    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      function: reloadDevices(),
-    });
-});
-
-connect.addEventListener("click", async () => {
+serverconn.addEventListener("click", async () => {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
-    function: connectDevice(),
+    function: sendServerID(),
   });
 });
 
-function connectDevice() {
-  var scales = document.getElementById('scales');
-  var value = scales.options[scales.selectedIndex].value;
-  chrome.runtime.sendMessage({ status: 'CONNECT', value: value });
+function checkIfServerIdExist() {
+  chrome.runtime.sendMessage({ status: 'CHECKIFCONNEXISTS' });
 }
 
-function reloadDevices() {
-  chrome.runtime.sendMessage({ status: 'RELOAD' });
+function sendServerID() {
+  var serverId = document.getElementById('serverid').value;
+  if (serverId == '') {
+    alert("Favor de rellenar el campo.")
+  } else {
+    chrome.runtime.sendMessage({ status: 'SERVERID', serverId: serverId });
+  }
 }
 
 chrome.runtime.onMessage.addListener(
   function (request, sender, sendResponse) {
     if (request) {
-      if (request.status == 'RELOAD') {
-        var scales = document.getElementById('scales');
-        var option;
-        var devices = request.devices;
-        connect.disabled = false;
-        scales.length = 0;
-        for (let i = 0; i < devices.length; i++) {
-          option = document.createElement('option');
-          option.text = devices[i].displayName;
-          option.value = devices[i].path;
-          scales.add(option);
-        }
+      if (request.status == 'SUCCESS') {
+        document.getElementById('serverid').remove();
+        document.getElementById('serverconn').remove();
+        var newDiv = document.createElement("div");
+        var newContent = document.createTextNode("¡Conectado!");
+        newDiv.appendChild(newContent); //añade texto al div creado.
+
+        // añade el elemento creado y su contenido al DOM
+        var currentDiv = document.getElementById("div1");
+        document.body.insertBefore(newDiv, currentDiv);
       }
-      if (request.status == 'RELOAD-EMPTY') {
-        var scales = document.getElementById('scales');
-        connect.disabled = true;
-        var option;
-        scales.length = 0;
-        option = document.createElement('option');
-        option.text = "No hay basculas conectadas";
-        option.value = "0";
-        scales.add(option);
+      if (request.status == 'EXISTS') {
+        document.getElementById('serverid').remove();
+        document.getElementById('serverconn').remove();
+        var newDiv = document.createElement("div");
+        var newContent = document.createTextNode("¡Conectado!");
+        newDiv.appendChild(newContent); //añade texto al div creado.
+
+        // añade el elemento creado y su contenido al DOM
+        var currentDiv = document.getElementById("div1");
+        document.body.insertBefore(newDiv, currentDiv);
       }
     }
   });
